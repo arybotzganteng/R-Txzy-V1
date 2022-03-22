a2247ba5809f823cedfa4edd13abb9153a3fd97f
arybotzganteng <101872515+arybotzganteng@users.noreply.github.com>
2022-03-23 04:24:31 +0800

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let res = await fetch(`https://api.lolhuman.xyz/api/pinterest?apikey=Dawnfrostkey&query=loli`)
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  conn.sendButtonImg(m.chat, await (await fetch(json.result)).buffer(), `Kyaa, , kawaii !   > / / / <`, `${wm}`, 'Next', `#loli`, m, 0, { thumbnail: await (await fetch(json.result)).buffer() })
-}
-handler.help = ['loli']
-handler.tags = ['anime']
-handler.command = /^(loli)$/i
 
+let handler = async (m, { conn, text }) => {
+let res = await fetch(' https://raw.githubusercontent.com/Alfarqun/database/main/loli.json ')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Loli', watermark, 'NEXT', '/loli', m)
+}
+handler.command = /^(loli)$/i
+handler.limit = true
+handler.tags = ['anime']
+handler.help = ['loli']
 module.exports = handler
-let wm = global.botwm
