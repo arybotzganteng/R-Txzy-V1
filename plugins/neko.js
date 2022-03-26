300df4c09882b896d98aff4ee8668212cea3fb2d
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-03-26 09:10:52 +0800

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -1,13 +1,15 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
-  if (!res.ok) throw eror
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', watermark, m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  if (!json.url) throw 'Error!'
+  conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Nyaa', wm, 'Next', `${usedPrefix + command}`, m,)
 }
 handler.help = ['neko']
-handler.tags = ['internet']
-handler.command = /^neko$/i
+handler.tags = ['anime']
+handler.command = /^(neko)$/i
 
 module.exports = handler
+
+let wm = global.botwm
