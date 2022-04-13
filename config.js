f19de6d4fdc750217a01323d710dcd6a83964c91
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-13 21:19:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['79519874382', '79519874382','79519874382', '79519874382', '0109629671'] // Letakan nomor kamu disini
+global.owner = ['79519874382', '0109629671','79519874382', '79519874382', '0109629671'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
